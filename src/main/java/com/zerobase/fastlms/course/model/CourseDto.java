c2710cb17e01bb5157eef6853954772b539a7356
Woong5026 <ktw5026@naver.com>
2022-04-20 16:39:22 +0900

    강좌 목록 구현 complete

--- src/main/java/com/zerobase/fastlms/course/model/CourseDto.java
+++ src/main/java/com/zerobase/fastlms/course/model/CourseDto.java
@@ -2,10 +2,24 @@ package com.zerobase.fastlms.course.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Data
 public class CourseDto {
 
+    Long id;
+    long categoryId;
+    String imagePath;
+    String keyword;
     String subject;
+    String summary;
+    String contents;
+    long price;
+    long salePrice;
+    Date saleEndDt;
+    LocalDateTime regDt;//등록일(추가날짜)
+    LocalDateTime udtDt;//수정일(수정날짜)
 
     // 페이징 카운트트
     long totalCount;
--- src/main/java/com/zerobase/fastlms/course/service/CourseServiceImpl.java
+++ src/main/java/com/zerobase/fastlms/course/service/CourseServiceImpl.java
@@ -1,19 +1,25 @@
 package com.zerobase.fastlms.course.service;
 
+import com.zerobase.fastlms.admin.dto.MemberDto;
 import com.zerobase.fastlms.admin.model.CategoryDto;
 import com.zerobase.fastlms.course.entity.Course;
+import com.zerobase.fastlms.course.mapper.CourseMapper;
 import com.zerobase.fastlms.course.model.CourseDto;
+import com.zerobase.fastlms.course.model.CourseParam;
 import com.zerobase.fastlms.course.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CourseServiceImpl implements CourseService{
 
     private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
 
     @Override
     public boolean add(CourseDto param) {
@@ -26,5 +32,25 @@ public class CourseServiceImpl implements CourseService{
 
 
         return true;
+    }
+
+    @Override
+    public List<CourseDto> list(CourseParam param) {
+
+        long totalCount = courseMapper.selectListCount(param);
+
+        List<CourseDto> list = courseMapper.selectList(param);
+
+        // 각 칼럼에 totalCount를 넣어주는 식
+        if(!CollectionUtils.isEmpty(list)){
+            int i = 0;
+            for (CourseDto m : list){
+                m.setTotalCount(totalCount);
+                m.setSeq(totalCount - param.getPageStart() - i);
+                i++;
+            }
+        }
+
+        return list;
     }
 }
