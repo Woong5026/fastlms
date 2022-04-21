b33ec1ebf46cf794600c81f14f1a32ed768c04a5
Woong5026 <ktw5026@naver.com>
2022-04-21 18:40:44 +0900

    스마트 에디터  complete

--- src/main/java/com/zerobase/fastlms/configuration/SecurityConfiguration.java
+++ src/main/java/com/zerobase/fastlms/configuration/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
 
         http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
